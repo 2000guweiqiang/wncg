0ad1a80e79b87f2fee32cb020e927b4e08980277
2000guweiqiang <guweiqiang22@163.com>
2018-11-17 15:43:36 +0800

    new program

---
+++ 水仙花.py
@@ -0,0 +1,9 @@
+ef number(x, y):
+	num = int(num)
+	x <= y and x >= 100 and y <= 1000
+	gewei = num % 10
+	baiwei = int(num / 100)
+	shiwei = int(num -baiwei * 100) / 10)
+	sum = geiwei * gewei * gewei + shiwei * shiiwei * shiwiei + baiwei * baiwei * baiwei
+    if (sum == nun):
+        print(num)
